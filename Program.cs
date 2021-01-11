f0f2bb33e81ace25dea96bcd96cc686a89581e0a
Caroline Richards <70033718+cobalt819@users.noreply.github.com>
2021-01-11 10:06:22 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Caroline, self-taught highschool freshman.");
+            Console.WriteLine("There's nowhere to go but up!");
+        }
+    }
+}
